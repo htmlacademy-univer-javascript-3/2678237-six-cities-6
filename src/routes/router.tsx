031d1dad95155281f4603daf5f8a60d81ea8a26e
dnomokonov <zmnomokon715@gmail.com>
2026-03-11 20:31:46 +0700

    Update page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,7 +3,7 @@ import App from '../App.tsx';
 import {MainPage} from '../pages/MainPage.tsx';
 import {LoginPage} from '../pages/LoginPage.tsx';
 import {OfferPage} from '../pages/OfferPage.tsx';
-import {NotFoundPage} from '../pages/NotFoundPage.tsx';
+import {NotFoundPage} from '../pages/NotFoundPage/NotFoundPage.tsx';
 import PrivateRoute from './privateRoute.tsx';
 import {AppRoute, AuthorizationStatus} from '../const.ts';
 import {FavoritesPage} from '../pages/FavoritesPage.tsx';
