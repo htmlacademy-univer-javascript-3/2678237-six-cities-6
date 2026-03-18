99af8d701ecf47a8a6667e5e5c4e965abbf4860b
dnomokonov <zmnomokon715@gmail.com>
2026-03-18 20:26:28 +0700

    Update mock

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -1,6 +1,15 @@
-import {Offers} from '../types/offer.ts';
+import {City, Offers} from '../types/offer.ts';
 
-export const offers : Offers = [
+export const AMSTERDAM_CITY : City = {
+  name: 'Amsterdam',
+  location: {
+    latitude: 52.37914938496378,
+    longitude: 4.900377537499948,
+    zoom: 12,
+  },
+};
+
+export const offers: Offers = [
   {
     id: '67318f9c-706d-44e2-9b27-0993a8306c51',
     title: 'Canal View Prinsengracht',
@@ -8,21 +17,21 @@ export const offers : Offers = [
     price: 478,
     previewImage: 'https://14.design.htmlacademy.pro/static/hotel/8.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
-        zoom: 13
-      }
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 12,
+      },
     },
     location: {
-      latitude: 48.868610000000004,
-      longitude: 2.342499,
-      zoom: 16
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+      zoom: 16,
     },
     isFavorite: false,
     isPremium: true,
-    rating: 1.5
+    rating: 1.5,
   },
   {
     id: 'a9d98153-0052-4c29-8aa6-03461af8eb4f',
@@ -31,21 +40,21 @@ export const offers : Offers = [
     price: 102,
     previewImage: 'https://14.design.htmlacademy.pro/static/hotel/15.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
-        zoom: 13
-      }
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 12,
+      },
     },
     location: {
-      latitude: 48.858610000000006,
-      longitude: 2.330499,
-      zoom: 16
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
+      zoom: 16,
     },
     isFavorite: true,
     isPremium: false,
-    rating: 3.6
+    rating: 3.6,
   },
   {
     id: 'a9b2a105-17e1-4a35-80f7-0f8790c2566e',
@@ -54,21 +63,21 @@ export const offers : Offers = [
     price: 187,
     previewImage: 'https://14.design.htmlacademy.pro/static/hotel/11.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
-        zoom: 13
-      }
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 12,
+      },
     },
     location: {
-      latitude: 48.834610000000005,
-      longitude: 2.335499,
-      zoom: 16
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 16,
     },
     isFavorite: false,
     isPremium: true,
-    rating: 1.6
+    rating: 1.6,
   },
   {
     id: '89bd250c-2443-4112-91ea-cc147fd65f63',
@@ -77,20 +86,20 @@ export const offers : Offers = [
     price: 150,
     previewImage: 'https://14.design.htmlacademy.pro/static/hotel/17.jpg',
     city: {
-      name: 'Dusseldorf',
+      name: 'Amsterdam',
       location: {
-        latitude: 51.225402,
-        longitude: 6.776314,
-        zoom: 13
-      }
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 12,
+      },
     },
     location: {
-      latitude: 51.225402,
-      longitude: 6.784314,
-      zoom: 16
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16,
     },
     isFavorite: true,
     isPremium: true,
-    rating: 3.8
-  }
+    rating: 3.8,
+  },
 ];
