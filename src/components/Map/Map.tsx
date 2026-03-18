03b986933ee69d97b54a9119b61a1f3d231feedf
dnomokonov <zmnomokon715@gmail.com>
2026-03-18 20:26:56 +0700

    Add Map

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,55 @@
+import {useEffect, useRef} from 'react';
+import {City, Offer, Offers} from '../../types/offer.ts';
+import useMap from '../../hooks/useMap.ts';
+import {Icon, layerGroup, Marker} from 'leaflet';
+import {URL_MARKER_CURRENT, URL_MARKER_DEFAULT} from '../../const.ts';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  city: City;
+  offers: Offers;
+  selectedOffer: Offer | null;
+}
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [30, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [30, 40],
+  iconAnchor: [20, 40]
+});
+
+export function Map({city, offers, selectedOffer}: MapProps) {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      const markerLayer = layerGroup().addTo(map);
+      offers.map((offer) => {
+        const marker = new Marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude,
+        });
+
+        marker.setIcon(
+          selectedOffer !== undefined && offer?.id === selectedOffer?.id
+            ? currentCustomIcon
+            : defaultCustomIcon
+        ).addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, offers, selectedOffer]);
+
+  return (
+    <div style={{height: '100%'}} ref={mapRef} />
+  );
+}
